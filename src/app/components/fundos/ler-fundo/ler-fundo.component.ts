94ccce7b9214ae662f796f58659309061f78fe59
Jhonatan Santos <48073565+jrsantos1@users.noreply.github.com>
2022-09-30 14:06:09 -0300

    adicionando filtro

--- src/app/components/fundos/ler-fundo/ler-fundo.component.ts
+++ src/app/components/fundos/ler-fundo/ler-fundo.component.ts
@@ -1,6 +1,7 @@
 import { Fundos } from './../fundos.module';
 import { FundoService } from './../fundo.service';
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-ler-fundo',
@@ -20,8 +21,41 @@ export class LerFundoComponent implements OnInit {
     }else{
       return 'yellow'
     }
+  }
+
+  filtro(){
+    const INPUT_BUSCA = document.getElementById('input-busca')
+    const TABELA_FUNDOS = document.getElementById('tabela-fundos')
+
+    INPUT_BUSCA?.addEventListener('keyup', () =>{
+
+      let expresao = (INPUT_BUSCA as HTMLInputElement).value;
+
+      let linhas = TABELA_FUNDOS?.getElementsByTagName('tr')
+
+      for(let posicao in linhas){
+        if(true === isNaN(Number(posicao))){
+          continue
+        }
+
+        let conteudo = linhas[Number(posicao)].innerHTML
+
+        if(true === conteudo.includes(expresao)) {
+          linhas[Number(posicao)].style.display = '';
+        }else{
+          linhas[Number(posicao)].style.display = 'none';
+        }
+
+
+      }
+
+      console.log(expresao)
+
+    })
 
   }
+
+
 
   fundos: Fundos[] | undefined
 
@@ -32,6 +66,8 @@ export class LerFundoComponent implements OnInit {
     //   this.fundos = fundos
     //   console.log(this.fundos)
     // })
+
+    this.filtro()
 
      this.fundosService.getAllMultimesas().subscribe(fundos => {
        this.fundos = fundos
